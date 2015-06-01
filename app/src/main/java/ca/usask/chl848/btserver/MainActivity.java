05ed453e3263f67dd3e2cc84fb323a9160b4f140
leslie0216 <leslie_lcz@hotmail.com>
2015-06-01 16:53:43 -0600

    add btserver project with demo version

---
+++ app/src/main/java/ca/usask/chl848/btserver/MainActivity.java
@@ -0,0 +1,462 @@
+package ca.usask.chl848.btserver;
+
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothServerSocket;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.DisplayMetrics;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.UUID;
+
+
+public class MainActivity extends Activity implements View.OnClickListener {
+
+    private MainView m_mainView;
+    //private int m_mainViewId;
+
+    private ServerThread m_serverThread = null;
+    private ArrayList<ConnectedThread> m_connectedThreadList = new ArrayList<>();
+
+    private BluetoothAdapter m_bluetoothAdapter = null;
+
+    private BluetoothServerSocket m_serverSocket = null;
+    private ArrayList<BluetoothSocket> m_socketList = new ArrayList<>();
+
+    private UUID m_UUID = UUID.fromString("8bb345b0-712a-400a-8f47-6a4bda472638");
+
+    private ArrayList<InputStream> m_inStreamList = new ArrayList<>();
+    private ArrayList<OutputStream> m_outStreamList = new ArrayList<>();
+
+    private static int REQUEST_ENABLE_BLUETOOTH = 1;
+
+    private ArrayList m_messageList = new ArrayList();
+
+    private boolean m_isConnected;
+
+    private int m_connection_count;
+
+    private boolean m_isOn;
+
+    long startTime = 0;
+
+    Handler timerHandler = new Handler();
+    Runnable timerRunnable = new Runnable() {
+        @Override
+        public void run() {
+            m_mainView.invalidate();
+            if (m_messageList.size() == 0) {
+                addMessage(m_mainView.cookMessage());
+            }
+
+            sendMessage();
+            timerHandler.postDelayed(this, 500);
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        setStatus("");
+        m_connection_count = 0;
+
+        Button btn=(Button)this.findViewById(R.id.btn_switch);
+        btn.setOnClickListener(this);
+
+        m_mainView = new MainView(this);
+
+        DisplayMetrics displayMetrics = getResources().getDisplayMetrics();
+        this.addContentView(m_mainView, new LinearLayout.LayoutParams(displayMetrics.widthPixels, (int) (displayMetrics.heightPixels * 0.7f)));
+
+        m_isOn = false;
+
+        startTime = System.currentTimeMillis();
+        timerHandler.postDelayed(timerRunnable, 0);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void setStatus(String status){
+        TextView v = (TextView)this.findViewById(R.id.txt_status_cnt);
+        v.setText(status + " : " + m_connectedThreadList.size());
+    }
+
+    @Override
+    protected void onResume() {
+        if (m_isOn) {
+            setupThread();
+        }
+        super.onResume();
+    }
+
+    @Override
+    protected void onRestart() {
+        if (m_isOn) {
+            setupThread();
+        }
+        super.onRestart();
+    }
+
+    @Override
+    protected void onDestroy() {
+        m_isOn = false;
+        stopThreads();
+        super.onDestroy();
+    }
+
+    private void stopThreads() {
+        if(m_bluetoothAdapter!=null&&m_bluetoothAdapter.isDiscovering()){
+            m_bluetoothAdapter.cancelDiscovery();
+        }
+
+        if (m_serverThread != null) {
+            m_serverThread.cancel();
+            m_serverThread = null;
+        }
+
+        int size = m_connectedThreadList.size();
+        if (size != 0) {
+            for (int i = 0; i<size; ++i) {
+                ConnectedThread ct = m_connectedThreadList.get(i);
+                if (ct != null) {
+                    ct.cancel();
+                    ct.m_isStoppedByServer = true;
+                }
+            }
+        }
+        m_connectedThreadList.clear();
+
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                setStatus("Not Connected");
+            }
+        });
+        //setStatus("Not Connected");
+        m_isOn = false;
+    }
+
+    @Override
+    public void onClick(View v) {
+        Button btn=(Button)this.findViewById(R.id.btn_switch);
+        if (m_isOn == false) {
+            setupBluetooth();
+            m_isOn = true;
+            btn.setText("Stop Server");
+        } else {
+            stopThreads();
+            m_isOn = false;
+            btn.setText("Start Server");
+        }
+    }
+
+    public void showToast(String message)
+    {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
+
+    private void setupBluetooth(){
+        setStatus("Not Connected");
+        m_isConnected = false;
+
+        m_bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        if(m_bluetoothAdapter != null){  //Device support Bluetooth
+            if(!m_bluetoothAdapter.isEnabled()){
+                Intent intent=new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(intent, REQUEST_ENABLE_BLUETOOTH);
+            }
+            else {
+                setupThread();
+            }
+        }
+        else{   //Device does not support Bluetooth
+
+            Toast.makeText(this,"Bluetooth not supported on device", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_ENABLE_BLUETOOTH) {
+            if (resultCode == RESULT_OK) {
+                setupThread();
+            }
+            else {
+                showToast("Bluetooth is not enable on your device");
+            }
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    public void setupThread(){
+        if (m_serverThread == null) {
+            m_serverThread = new ServerThread();
+            m_serverThread.start();
+        }
+    }
+
+    private class ConnectedThread extends Thread {
+        BluetoothSocket m_connectedSocket;
+        InputStream m_inStream;
+        OutputStream m_outStream;
+        String m_deviceName;
+        boolean m_isStoppedByServer;
+        public ConnectedThread(BluetoothSocket socket) {
+            try {
+                m_connectedSocket = socket;
+                m_inStream = m_connectedSocket.getInputStream();
+                m_outStream = m_connectedSocket.getOutputStream();
+                m_deviceName = "";
+                //setStatus("Connected");
+                m_isConnected = true;
+                m_isStoppedByServer = false;
+            } catch (IOException e){
+
+            }
+        }
+
+        @Override
+        public void run() {
+
+            byte[] buffer = new byte[1024];
+            int bytes;
+
+            while (true) {
+                try {
+                    // Read from the InputStream
+                    if( m_inStream != null && (bytes = m_inStream.read(buffer)) > 0 )
+                    {
+                        byte[] buf_data = new byte[bytes];
+                        for(int i=0; i<bytes; i++)
+                        {
+                            buf_data[i] = buffer[i];
+                        }
+                        String msg = new String(buf_data);
+                        String id = receiveBTMessage(msg);
+                        if (m_deviceName.equalsIgnoreCase("")) {
+                            m_deviceName = id;
+                        }
+                    }
+                } catch (IOException e) {
+                    cancel();
+                    if (m_isStoppedByServer == false) {
+                        m_connectedThreadList.remove(this);
+                    }
+                    m_mainView.removeDevice(m_deviceName);
+                    if (m_connectedThreadList.isEmpty()) {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                setStatus("Not Connected");
+                            }
+                        });
+                    } else {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                setStatus("Connected");
+                            }
+                        });
+                    }
+                    //setStatus("Not Connected");
+                    //m_isConnected = false;
+                    m_connection_count--;
+                    break;
+                }
+            }
+        }
+
+        public void write(String msg) {
+            try {
+                if (m_outStream != null) {
+                    m_outStream.write(msg.getBytes());
+                }
+            } catch (IOException e) {
+
+            }
+        }
+
+        public void cancel() {
+            try {
+                if (m_inStream != null) {
+                    m_inStream.close();
+                    m_inStream = null;
+                }
+                if (m_outStream != null) {
+                    m_outStream.flush();
+                    m_outStream.close();
+                    m_outStream = null;
+                }
+                if (m_connectedSocket != null) {
+                    m_connectedSocket.close();
+                    m_connectedSocket = null;
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private class ServerThread extends Thread {
+        public ServerThread () {
+            try {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        setStatus("Connecting...");
+                    }
+                });
+                //setStatus("Connecting...");
+                m_serverSocket = m_bluetoothAdapter.listenUsingRfcommWithServiceRecord("BTServer", m_UUID);
+            } catch (IOException e) {
+                m_serverSocket = null;
+            }
+        }
+        @Override
+        public void run() {
+            // Keep listening until exception occurs or a socket is returned
+            while (true) {
+                BluetoothSocket socket;
+                if (m_serverSocket != null) {
+                    try {
+                        socket = m_serverSocket.accept();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                        break;
+                    }
+                }
+                else {
+                    try {
+                        m_serverSocket = m_bluetoothAdapter.listenUsingRfcommWithServiceRecord("BTServer", m_UUID);
+                    } catch (IOException e) {
+                        m_serverSocket = null;
+                    }
+                    continue;
+                }
+
+                if (socket != null) {
+
+                    m_socketList.add(socket);
+                    ConnectedThread ct = new ConnectedThread(socket);
+                    ct.start();
+                    m_connectedThreadList.add(ct);
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            setStatus("Connected");
+                        }
+                    });
+                }
+            }
+        }
+
+        public void cancel() {
+            try {
+                if (m_serverSocket != null) {
+                    m_serverSocket.close();
+                }
+            } catch (IOException e) {
+            }
+        }
+    }
+
+    public void sendBTMessage(String msg) {
+        if (!m_connectedThreadList.isEmpty()) {
+            int size = m_connectedThreadList.size();
+
+            for (int i = 0; i<size; ++i) {
+                m_connectedThreadList.get(i).write(msg);
+            }
+        }
+    }
+
+    public void addMessage(String msg) {
+        m_messageList.add(msg);
+    }
+
+    public void sendMessage(){
+        if (m_messageList.size() != 0) {
+            String msg = (String)m_messageList.get(0);
+            m_messageList.remove(0);
+            sendBTMessage(msg);
+        }
+    }
+
+    private String receiveBTMessage(String msg){
+        String rt = "";
+        try {
+            JSONObject jsonObject = new JSONObject(msg);
+
+            String id = jsonObject.getString("id");
+            float senderX = (float) jsonObject.getDouble("x");
+            float senderY = (float) jsonObject.getDouble("y");
+            float senderZ = (float) jsonObject.getDouble("z");
+            int color = jsonObject.getInt("color");
+
+            String receiverId = "";
+            String ballId = "";
+            int ballColor = 0;
+
+            boolean isSendingBall = jsonObject.getBoolean("isSendingBall");
+            if (isSendingBall == true) {
+                receiverId = jsonObject.getString("receiverId");
+                ballId = jsonObject.getString("ballId");
+                ballColor = jsonObject.getInt("ballColor");
+            }
+
+            m_mainView.updateClientInfo(id, color, senderX, senderY, senderZ, isSendingBall, ballId, ballColor, receiverId);
+
+            rt = id;
+        }catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return rt;
+    }
+
+    public boolean isConnected(){
+        return m_isConnected;
+    }
+}
---
+++ app/src/main/java/ca/usask/chl848/btserver/MainView.java
@@ -0,0 +1,122 @@
+package ca.usask.chl848.btserver;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.DisplayMetrics;
+import android.view.View;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by chl848 on 05/11/2015.
+ */
+public class MainView extends View {
+
+    Paint m_paint;
+
+    private String m_status;
+
+    public class ClientInfo {
+        float x;
+        float y;
+        float z;
+        int color;
+
+        boolean isSendingBall;
+        String ballId;
+        int ballColor;
+        String receiverId;
+    }
+    private Map<String, ClientInfo> m_clients = new HashMap<>();
+
+    public MainView(Context context) {
+        super(context);
+
+        m_paint = new Paint();
+    }
+
+    public void updateClientInfo(String id, int color, float x, float y, float z, boolean isSendingBall, String ballId, int ballColor, String receiverId) {
+        ClientInfo clientInfo = new ClientInfo();
+        clientInfo.x = x;
+        clientInfo.y = y;
+        clientInfo.z = z;
+        clientInfo.color = color;
+        clientInfo.isSendingBall = isSendingBall;
+        clientInfo.ballId = ballId;
+        clientInfo.ballColor = ballColor;
+        clientInfo.receiverId = receiverId;
+        m_clients.put(id, clientInfo);
+    }
+
+    public String cookMessage() {
+        JSONArray msg = new JSONArray();
+        for(Map.Entry<String,ClientInfo> entry : m_clients.entrySet()){
+            JSONObject record = new JSONObject();
+            String strkey = entry.getKey();
+            ClientInfo strval = entry.getValue();
+            try {
+                record.put("id", strkey);
+                record.put("color", strval.color);
+                record.put("x", strval.x);
+                record.put("y", strval.y);
+                record.put("z", strval.z);
+                record.put("isSendingBall", strval.isSendingBall);
+                record.put("ballId", strval.ballId);
+                record.put("ballColor", strval.ballColor);
+                record.put("receiverId", strval.receiverId);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            msg.put(record);
+        }
+
+        return msg.toString();
+    }
+
+    public void setStatus(String status) {
+        m_status = status;
+    }
+
+    @Override
+    protected  void onDraw(Canvas canvas) {
+        showConnections(canvas);
+    }
+
+    private void showConnections(Canvas canvas) {
+        DisplayMetrics displayMetrics = getContext().getResources().getDisplayMetrics();
+
+        m_paint.setTextSize(30);
+        m_paint.setColor(Color.RED);
+
+        float x = 0.05f;
+        float y = 0.2f;
+
+        for(Map.Entry<String,ClientInfo> entry : m_clients.entrySet()){
+            String strkey = entry.getKey();
+            ClientInfo strval = entry.getValue();
+            String output = strkey + " - Angle : " + strval.z;
+            canvas.drawText(output, (int) (displayMetrics.widthPixels * x), (int) (displayMetrics.heightPixels * y), m_paint);
+            y += 0.05f;
+        }
+    }
+
+    private void showStatus(){
+        MainActivity ma = (MainActivity)getContext();
+
+        ma.setStatus(m_status);
+    }
+
+    public void removeDevice(String id) {
+        if (m_clients.containsKey(id) == true) {
+            m_clients.remove(id);
+        }
+    }
+}
